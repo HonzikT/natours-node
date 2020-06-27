6ab91f32c848e6f8fa04db2f3f28c450e138cf03
HonzikT <truxajan@gmail.com>
2020-06-28 01:02:01 +0200

    implemented query string optionsinto APIFeatures class - filtering, sorting, limiting, pagination and aliasing; implemented getTourStats and getMonthlyPlan aggregation pipelines

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,22 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+
+  next()
+}
+
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Executing the query
+    const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate();
+    const tours = await features.query;
 
+    // Response
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -92,3 +105,69 @@ exports.deleteTour = async (req, res) => {
   }
 };
 
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      { $match: { ratingsAverage: { $gte: 4.5 } } },
+      { $group: {
+        _id: '$difficulty',
+        numTours: { $sum: 1 },
+        numRatings: { $sum: '$ratingsQuantity' },
+        avgRating: { $avg: '$ratingsAverage' },
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' }
+      } },
+      { $sort: { avgPrice: 1 } }
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats
+      }
+    });
+
+  } catch(err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = Number(req.params.year);
+
+    const plan = await Tour.aggregate([
+      { $unwind: '$startDates' },
+      { $match: { startDates: { $gte: new Date(`${year}-01-01`),
+                                $lte: new Date(`${year}-12-31`)}
+      } },
+      { $group: { 
+        _id: { $month: '$startDates' },
+        numTourStarts: { $sum: 1 },
+        tours: { $push: '$name' }
+      } },
+      { $addFields: { month: '$_id' } },
+      { $project: { _id: 0 } },
+      { $sort: { numTourStarts: -1 }},
+      { $limit: 12 }
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan
+      }
+
+    });
+  } catch(err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+}
