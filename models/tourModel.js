8ff36e208400b85872fc330d602e51a1d24b7f3c
HonzikT <truxajan@gmail.com>
2020-06-28 15:06:10 +0200

    Implemented tour query and aggregation middleware regarding the secret tours, updated tourModel with basic data validation

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
-    trim: true
+    trim: true,
+    maxlength: [40, 'A tour name must have max 40 characters'],
+    minlength: [10, 'A tour name must have min 10 characters']
   },
   slug: String,
   duration: {
@@ -19,11 +22,17 @@ const tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String,
-    required: [true, 'A tour must have a difficulty']
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult'
+    }
   },
   ratingsAverage: {
     type: Number,
-    default: 4.5
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0']
   },
   ratingsQuantity: {
     type: Number,
@@ -33,7 +42,15 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price']
   },
-  priceDiscount: Number,
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function(val) {
+        return val < this.price;
+      },
+      message: 'Discount price of ({VALUE}) has to be lower than regular price'
+    }
+  },
   summary: {
     type: String,
     trim: true,
@@ -53,7 +70,11 @@ const tourSchema = new mongoose.Schema({
     default: Date.now(),
     select: false
   },
-  startDates: [Date]
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false
+  }
 }, {
   // Second schema parameter, ensures that virtual properties are included
   toJSON: { virtuals: true },
@@ -70,6 +91,18 @@ tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next()
 });
+
+// Query middleware
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } })
+  next();
+});
+
+// Aggregation middleware
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true }} });
+  next();
+})
 
 const Tour = mongoose.model('Tour', tourSchema);
 
