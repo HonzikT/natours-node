c7413cc4b4a107f5021c0debbe75758cfe07149b
HonzikT <truxajan@gmail.com>
2021-01-20 20:55:53 +0100

    unhandled and uncought exceptions

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', (err) => {
+  console.error(err.name, err.message);
+  process.exit(1);
+});
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -20,6 +25,15 @@ mongoose
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App running on port: ${PORT}`); // eslint-disable-line no-console
+});
+
+process.on('unhandledRejection', (err) => {
+  console.error(err.name, err.message);
+
+  // Enables to complete previous requests before the server stops
+  server.close(() => {
+    process.exit(1);
+  });
 });
